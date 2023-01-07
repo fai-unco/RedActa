0a38fc44425c356433484f3c59ce47db8d85b328
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2023-01-07 00:13:50 -0300

    feat: one to one relationship between document and document_copy

--- api/app/Models/Document.php
+++ api/app/Models/Document.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Document extends Model
 {
     use HasFactory;
+
+    public function document_copy()
+    {
+        return $this->hasOne(Document_copy::class);
+    }
 }
--- api/app/Models/Document_copy.php
+++ api/app/Models/Document_copy.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Document_copy extends Model
 {
     use HasFactory;
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
+
 }
