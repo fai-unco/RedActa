226f1c1f0181dd53cd6c7c666f6e8960f6d93881
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2023-01-10 12:08:24 -0300

    feat: document table migration

---
+++ api/database/migrations/2023_01_10_142451_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('type');
+            $table->string('content');
+            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('redacta_users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
