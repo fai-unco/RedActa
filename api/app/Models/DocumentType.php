747b490fe5ea956862dca3e49a31a88bad5cbe2b
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2023-01-12 17:35:51 -0300

    add: model and relationship has many

---
+++ api/app/Models/DocumentType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class DocumentType extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'description',
+    ];
+
+
+    //hasMany
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
+}
