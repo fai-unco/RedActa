6a69bf31520a8c3044d20093d5554f945f94982e
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-01-17 21:06:07 -0300

    change sidebar menu items

--- frontend/src/app/panel/sidebar-menu/sidebar-menu.component.ts
+++ frontend/src/app/panel/sidebar-menu/sidebar-menu.component.ts
@@ -11,76 +11,10 @@ export class SidebarMenuComponent implements OnInit {
 
   items: NbMenuItem[]  = [
     {
-      title: 'Declaraciones',
+      title: 'Crear documento',
+      link: ''
       //expanded: true,
-      children: [
-        {
-          title: 'Crear',
-        },
-        {
-          title: 'Editar',
-        },
-        {
-          title: 'Subir dec. firmada',
-        },
-      ],
-    },
-    {
-      title: 'Resoluciones',
-      children: [
-        {
-          title: 'Crear',
-        },
-        {
-          title: 'Editar',
-        },
-        {
-          title: 'Subir firmada',
-        },
-      ],
-    },
-    {
-      title: 'Notas',
-      children: [
-        {
-          title: 'Accion 1',
-        },
-        {
-          title: 'Accion 2',
-        },
-        {
-          title: 'Accion 3',
-        },
-      ],
-    },
-    {
-      title: 'Memos',
-      children: [
-        {
-          title: 'Accion 1',
-        },
-        {
-          title: 'Accion 2',
-        },
-        {
-          title: 'Accion 3',
-        },
-      ],
-    },
-    {
-      title: 'Actas',
-      children: [
-        {
-          title: 'Accion 1',
-        },
-        {
-          title: 'Accion 2',
-        },
-        {
-          title: 'Accion 3',
-        },
-      ],
-    },
+    }
   ];
 
   constructor() { }
