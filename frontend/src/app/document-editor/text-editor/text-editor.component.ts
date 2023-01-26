b7f28b71dc0b63f791964b325f520833b9835653
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-01-26 11:17:51 -0300

    add form control support to text editor

--- frontend/src/app/document-editor/text-editor/text-editor.component.ts
+++ frontend/src/app/document-editor/text-editor/text-editor.component.ts
@@ -2,9 +2,11 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  Input,
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import Quill from 'quill';
 import QuillCursors from 'quill-cursors';
 
@@ -12,28 +14,41 @@ import QuillCursors from 'quill-cursors';
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.scss'],
-})
-export class TextEditorComponent implements AfterViewInit {
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting: TextEditorComponent
+    }]
+  })
+export class TextEditorComponent implements AfterViewInit, ControlValueAccessor {
+  @Input('placeholder') placeholder: string = "";
   @ViewChild('container') container!: ElementRef;
+  quillInstance: any;
+  onChange = (content: any) => {};
+  onTouched = () => {};
+  touched = false;
+  disabled = false;
+  content: string ="";
 
   constructor() {}
 
   ngAfterViewInit(): void {
     Quill.register('modules/cursors', QuillCursors);
-    const quill = new Quill(this.container.nativeElement, {
+    this.quillInstance = new Quill(this.container.nativeElement, {
       modules: {
         cursors: true,
         toolbar: [
           // adding some basic Quill content features
           //[{ header: [1, 2, false] }],
-          ['bold', 'italic'],
+          ['bold', 'italic', 'underline'],
 
           [
             //{ 'list': 'ordered'},
             { list: 'bullet' },
           ],
           //['link'],
-          //, 'underline'],
+          
           //[{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }]
         ],
         history: {
@@ -42,8 +57,50 @@ export class TextEditorComponent implements AfterViewInit {
           userOnly: true,
         },
       },
-      placeholder: 'Start writing',
+      placeholder: this.placeholder,
       theme: 'snow', // 'bubble' is also great
     });
+
+    this.quillInstance.on('text-change', (delta: any, oldDelta: any, source: any) => {
+      this.onTextChange();
+    });
+
+    this.quillInstance.root.innerHTML = this.content;
+  }
+
+  private onTextChange(){
+    this.content = this.quillInstance.root.innerHTML;
+    this.markAsTouched();
+    if (!this.disabled) {
+      this.onChange(this.content);
+    }
+  }
+
+  writeValue(content: any) {
+    this.content = content;
+    if(this.quillInstance?.root){
+      this.quillInstance.root.innerHTML = content;
+    }
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
   }
 }
+
+
