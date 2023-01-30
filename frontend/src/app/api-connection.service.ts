6b8e02c471ec81f0278d99f1458c94e808184072
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-01-30 19:22:04 -0300

    add api connection service

---
+++ frontend/src/app/api-connection.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiConnectionService {
+
+  constructor(private http: HttpClient) { }
+
+  get(urlSuffix: string, id?: string){
+    let url = environment.API_URL_BASE + urlSuffix;
+    if(id) {
+      url = `${url}/${id}`
+    }
+    return this.http.get(url);
+  }
+
+  post(urlSuffix: string, data: any){
+    return this.http.post(`${environment.API_URL_BASE}/${urlSuffix}`, data);
+  }
+
+  patch(urlSuffix: string, id: string, data: any){
+    return this.http.patch(`${environment.API_URL_BASE}/${urlSuffix}/${id}`, data);
+  }
+
+  delete(urlSuffix: string, id: string){
+    return this.http.delete(`${environment.API_URL_BASE}/${urlSuffix}/${id}`);
+  }
+
+
+}
