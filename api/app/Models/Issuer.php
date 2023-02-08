31761c64f914cbb83129525e1a7104ede9516f08
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-02-08 08:32:48 -0300

    add issuer model and migration for creating issuers table

---
+++ api/app/Models/Issuer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Issuer extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'description'
+    ];
+
+    public function documents(){
+        return $this->hasMany(Document::class);
+    }
+}
