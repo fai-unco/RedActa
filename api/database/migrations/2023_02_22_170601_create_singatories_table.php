4e796f966653a230426735e0763bc634d24b4373
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2023-03-01 17:28:17 -0300

    add: migration

---
+++ api/database/migrations/2023_02_22_170601_create_singatories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('singatories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->timestamps();
+        });
+        Schema::table('singatories', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('singatories');
+    }
+};
