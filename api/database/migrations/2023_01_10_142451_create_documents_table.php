b1523d46e3957babc0069ef08ee8fecca2c80d98
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2023-03-01 17:37:19 -0300

    refactor: comment out the foreign keys

--- api/database/migrations/2023_01_10_142451_create_documents_table.php
+++ api/database/migrations/2023_01_10_142451_create_documents_table.php
@@ -24,19 +24,19 @@ return new class extends Migration
             $table->string('subject')->nullable();
             $table->boolean('ad_referendum')->default(false);
 
-            $table->bigInteger('redacta_user_id')->unsigned();
-            $table->foreign('redacta_user_id')->references('id')->on('redacta_users');
-            $table->bigInteger('document_type_id')->unsigned();
-            $table->foreign('document_type_id')->references('id')->on('document_types');
-            $table->bigInteger('document_copy_id')->unsigned();
-            $table->foreign('document_copy_id')->references('id')->on('document_copies');
-            $table->bigInteger('issuer_id')->unsigned();
-            $table->foreign('issuer_id')->references('id')->on('issuers');
-            $table->string('issue_place');
-            $table->date('issue_date');
-            $table->string('destinatary')->nullable();
-            $table->string('subject')->nullable();
-            $table->boolean('ad_referendum')->default(false);
+            // $table->bigInteger('redacta_user_id')->unsigned();
+            // $table->foreign('redacta_user_id')->references('id')->on('redacta_users');
+            // $table->bigInteger('document_type_id')->unsigned();
+            // $table->foreign('document_type_id')->references('id')->on('document_types');
+            // $table->bigInteger('document_copy_id')->unsigned();
+            // $table->foreign('document_copy_id')->references('id')->on('document_copies');
+            // $table->bigInteger('issuer_id')->unsigned();
+            // $table->foreign('issuer_id')->references('id')->on('issuers');
+            // $table->string('issue_place');
+            // $table->date('issue_date');
+            // $table->string('destinatary')->nullable();
+            // $table->string('subject')->nullable();
+            // $table->boolean('ad_referendum')->default(false);
 
         });
         Schema::table('documents', function (Blueprint $table) {
