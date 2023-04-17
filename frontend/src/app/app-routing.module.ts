26b56eebec56e96eb6e03ca47eea26b94dc50b76
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2023-04-17 14:56:07 -0300

    (front) set auth guard for all routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { 
     path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
   },
   { 
-    path: '', loadChildren: () => import('./panel/panel.module').then(m => m.PanelModule) 
+    path: '', loadChildren: () => import('./panel/panel.module').then(m => m.PanelModule),
+    canActivate: [AuthGuard] 
   },
 ]
 @NgModule({
