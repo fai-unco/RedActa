a1b4cbcdebcb5820bd9a1470255e487dc48ed51f
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2023-09-19 11:34:51 -0300

    (api): add new attibutes to fillable list in Issuer model

--- api/app/Models/Issuer.php
+++ api/app/Models/Issuer.php
@@ -11,10 +11,22 @@ class Issuer extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'description'
+        'description',
+        'phone',
+        'email',
+        'city',
+        'province',
+        'website_url'
     ];
 
     public function documents(){
         return $this->hasMany(Document::class);
+    }
+
+    public function set($data){
+        foreach ($data as $key => $value) {
+            $this->setAttribute($key, $value);
+        }
+        $this->save();
     }
 }
