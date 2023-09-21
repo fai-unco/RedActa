86147d3bfe2413c8abb4517de9f83007021404bf
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2023-09-21 11:37:18 -0300

    (api) improve request validation and error messages in AuthenticationController

--- api/app/Http/Controllers/AuthenticationController.php
+++ api/app/Http/Controllers/AuthenticationController.php
@@ -17,84 +17,92 @@ class AuthenticationController extends Controller
      */
     public function register (Request $request)
     {
+        $validatedData = $this->validateRequest($request);
         try {
-            // Se valida los datos que viene en $request
-            $validator = Validator::make($request->all(), [
-                'name' => 'required|string|max:255',        
-                'last_name' => 'required|string|max:255',        
-                'email' => 'required|email|unique:redacta_users|max:255',              
-                'password' => 'required|confirmed|min:8',    
-            ]); 
-            
-            // Si ocurre algun error en la validacion, retorna informacion sobre el error  
-            if ($validator->fails()) {       
-                $errors = $validator->errors();        
-                return response()->json(['errors' => $errors], 400);    
-            }    
-            
-           // Si la validacion es exitosa, crea el usuario y el token de acceso personal para el usuario. Retorna el token
-            if ($validator->passes()) {
-                $user = User::create([
-                    'name' => $request->name,
-                    'last_name' => $request->last_name,
-                    'email' => $request->email,
-                    'password' => Hash::make($request->password)       
-                ]);
-                return response()->json([
-                    'message' => 'Registration OK',
-                ], 200);
-            }
-            } catch (\Throwable $th) {
-                return response()->json(['message' => $th->getMessage()], 500);
-            }
+            $user = User::create([
+                'name' => $request->name,
+                'last_name' => $request->last_name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)       
+            ]);
+            return response()->json([
+                'status' => 201,
+                'message' => 'OK',
+                'data' => $user
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
         }
+    }
 
-        /**
-         * Maneja la peticion de inicio de sesión
-         * 
-         */
-        public function login (Request $request)
-        {
-          try {
-            //Valida las credenciales del usuario
+    /**
+     * Maneja la peticion de inicio de sesión
+     * 
+     */
+    public function login (Request $request)
+    {
+        try {
             if (!Auth::attempt($request->only('email', 'password'))){
                 return response()->json([
+                    'status' => 401,
                     'message' => 'Email y/o contraseña inválidos'
                 ], 401);
             }
-      
-            //Busca al usuario en la base de datos
             $user = User::where('email', $request['email'])->firstOrFail();
-
-            //Actualiza fecha y hora de último acceso
             $user->last_access = now();
             $user->save();
-      
-            //Genera un nuevo token para el usuario
             $token = $user->createToken('auth_token')->plainTextToken;
-      
-            //Retorna un JSON con el token generado y el tipo de token
             return response()->json([
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-                'username' => $user->name.' '.$user->last_name
+                'status' => 200,
+                'message' => 'OK',
+                'data' => [
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'username' => $user->name.' '.$user->last_name
+                ]
             ], 200);
-          } catch (\Throwable $th) {
-              return response()->json(['message' => $th->getMessage()], 500);
-          }
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
         }
-      
-        /**
-         * Maneja la peticion de cierre de sesión
-         * 
-         */
-        public function logout (Request $request)
-        {
-          try {
+    }
+    
+    /**
+     * Maneja la peticion de cierre de sesión
+     * 
+     */
+    public function logout (Request $request)
+    {
+        try {
             $request->user()->currentAccessToken()->delete();
-          } catch (\Throwable $th) {
-              return response()->json(['message' => $th->getMessage()], 500);
-          }
+        } catch (\Throwable $th) {
+            return response()->json(['message' => $th->getMessage()], 500);
         }
+    }
+
+
+    private function validateRequest($request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',        
+            'last_name' => 'required|string|max:255',        
+            'email' => 'required|email|unique:redacta_users',              
+            'password' => 'required|confirmed|min:8',  
+            //'roleId' => 'sometimes|numeric'
+        ], [
+            'required' => 'El campo :attribute es requerido',
+            'numeric' => 'El campo :attribute debe ser un número',
+            'string' => 'El campo :attribute debe ser un string',
+            'password.confirmed' => 'La contraseña ingresada y su confirmación no coinciden',
+            'password.min' => 'La contraseña ingresada debe tener una longitud no menor a 8 caracteres',
+            'email.unique' => 'El email ingresado ya existe. Intente con otro distinto',
+            'max'=> 'El valor del campo :attribute debe tener una longitud inferior a 255 caracteres'
+        ], [
+            'email' => '"Email"',
+            'name' => '"Nombre"',
+            'last_name' => '"Apellido"',
+            'password' => '"Contraseña"',
+            //'roleId' => '"Rol"',
+        ])->stopOnFirstFailure(true);
+        return $validator->validate();
+    }
 }
 
