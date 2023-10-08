34cd9e2c0635d0f0f65b3004c6177b49de6498d0
Federico Nahuel <f.nahuel.mamani@gmail.com>
2023-10-08 19:48:45 -0300

    (aoi) add migration to create operative_section_beginnings_table

---
+++ api/database/migrations/2023_10_08_183359_create_operative_section_beginnings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operative_section_beginnings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('content');
+            $table->bigInteger('issuer_id')->unsigned();
+            $table->foreign('issuer_id')->references('id')->on('issuers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operative_section_beginnings');
+    }
+};
