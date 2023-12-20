04c45e2bf8f180523a30ee203539e2aa639d5916
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2023-12-20 16:33:13 -0300

    (api) add column 'description' to 'stamps' table

--- api/database/migrations/2023_08_03_121706_create_stamps_table.php
+++ api/database/migrations/2023_08_03_121706_create_stamps_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('position')->nullable();
             $table->bigInteger('redacta_user_id')->unsigned();
             $table->foreign('redacta_user_id')->references('id')->on('redacta_users');
+            $table->string('description');
         });
 
         Schema::table('stamps', function (Blueprint $table) {
