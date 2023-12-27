ec5ac20ac5e705fc70a2748736c021c08aaa4990
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2023-12-27 08:02:14 -0300

    (front) fix auth interceptor, only redirect to login view if response status is 401 and the actual route is not '/login'

--- frontend/src/app/auth/auth.interceptor.ts
+++ frontend/src/app/auth/auth.interceptor.ts
@@ -8,19 +8,20 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, of, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     //handle your auth error or rethrow
-    if (err.status === 401 || err.status === 403) {
-        this.authService.logout();
-        // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
-        return of();
-      }
+    if ((err.status === 401 || err.status === 403) && this.router.url != '/login') {
+      this.authService.logout();
+      // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+      return of();
+    }
     return throwError(()=> err);
   }
 
