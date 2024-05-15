898a48cacee92dfbcd1fbf4ce3a9c9624ab7099c
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2024-05-15 14:17:03 -0300

    (front) add 'item selector' dialog

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbButtonModule, NbCardModule, NbDialogModule, NbIconModule, NbInputModule, NbListModule, NbSpinnerModule } from '@nebular/theme';
+import { NbAutocompleteModule, NbButtonModule, NbCardModule, NbDialogModule, NbIconModule, NbInputModule, NbListModule, NbSelectModule, NbSpinnerModule } from '@nebular/theme';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 import { TextEditorComponent } from './text-editor/text-editor.component';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { PageContainerComponent } from './page-container/page-container.component';
 import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { ItemSelectorComponent } from './item-selector/item-selector.component';
 
 @NgModule({
   declarations: [
     TextEditorComponent,
     ErrorDialogComponent,
     DeleteDialogComponent,
-    PageContainerComponent
+    PageContainerComponent,
+    ItemSelectorComponent
   ],
   imports: [
     CommonModule,
@@ -26,13 +28,16 @@ import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
     NbSpinnerModule,
     NbListModule,
     NbDialogModule.forChild(),
-    EditorModule
+    EditorModule,
+    NbSelectModule,
+    NbAutocompleteModule,
   ],
   exports: [
     TextEditorComponent,
     ErrorDialogComponent,
     DeleteDialogComponent,
-    PageContainerComponent
+    PageContainerComponent,
+    ItemSelectorComponent
   ],
   providers: [
     { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' },
