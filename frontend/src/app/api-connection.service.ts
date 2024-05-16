41f7ac3e71b78a512e8e9cdddaedc202d9ef285c
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2024-05-16 15:53:07 -0300

    (front) update 'patch' method from ApiConnectionService to support 'nested resources'

--- frontend/src/app/api-connection.service.ts
+++ frontend/src/app/api-connection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -10,23 +11,27 @@ export class ApiConnectionService {
 
   constructor(private http: HttpClient) { }
 
-  get(urlSuffix: string, id?: string, options:any = {headers: {accept:'application/json'}}){
+  get(urlSuffix: string, id?: string, options: any = {headers: {accept:'application/json'}}) {
     let url = `${environment.API_URL_BASE}/${urlSuffix}`;
-    if(id) {
+    if (id) {
       url = `${url}/${id}`
     }
     return this.http.get(url, options);
   }
 
-  post(urlSuffix: string, data: any){
+  post(urlSuffix: string, data: any) {
     return this.http.post(`${environment.API_URL_BASE}/${urlSuffix}`, data);
   }
 
-  patch(urlSuffix: string, id: string, data: any){
-    return this.http.patch(`${environment.API_URL_BASE}/${urlSuffix}/${id}`, data);
+  patch(resource: string, id: number, data: any, nestedResource?: string): Observable<any> {
+    let url = `${environment.API_URL_BASE}/${resource}/${id}`;
+    if (nestedResource) {
+      url = `${url}/${nestedResource}`;
+    }
+    return this.http.patch(url, data);
   }
 
-  delete(urlSuffix: string, id: string){
+  delete(urlSuffix: string, id: string) {
     return this.http.delete(`${environment.API_URL_BASE}/${urlSuffix}/${id}`);
   }
 
