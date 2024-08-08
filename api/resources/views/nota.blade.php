1eafba59a3e2e5e9a4df66443fdd5d125b7e102e
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2024-08-08 13:02:14 -0300

    (api) fix 'anexos' numeration in exported documents

--- api/resources/views/nota.blade.php
+++ api/resources/views/nota.blade.php
@@ -4,6 +4,7 @@
 @php( $body = json_decode($document->body) )
 @php( $stamps = json_decode($document->stamps) )
 @php( $hasAnexoUnico = $document->has_anexo_unico )
+@php( $intToRomanNumbers = [1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X'])
 
 <!DOCTYPE html>
 <html>
