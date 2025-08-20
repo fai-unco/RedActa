134696c7fd65a424d506ed702ac648141dfdd577
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-08-20 10:52:38 -0300

    (api) add form requests for RedactaUser model

---
+++ api/app/Http/Requests/StoreRedactaUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRedactaUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',        
+            'last_name' => 'required|string|max:255',        
+            'email' => 'required|email|unique:redacta_users',              
+            'password' => 'required|confirmed|min:8',
+            'role' => 'required|string|in:super_admin,local_admin,regular_user'
+        ];
+    }
+}
---
+++ api/app/Http/Requests/UpdateRedactaUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRedactaUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'sometimes|string|max:255',
+            'last_name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:redacta_users,email,'.$redactaUser->id,
+            'password' => 'sometimes|string|min:8|confirmed',
+            'role' => 'sometimes|string|in:super_admin,local_admin,regular_user'
+        ];
+    }
+}
