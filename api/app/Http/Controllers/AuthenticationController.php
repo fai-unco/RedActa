a1575ad2c5d2d01285e60548955b4d53aa58f76e
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-08-20 11:07:27 -0300

    (api) use form request and policy in AuthenticationController

--- api/app/Http/Controllers/AuthenticationController.php
+++ api/app/Http/Controllers/AuthenticationController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreRedactaUserRequest;
 
 class AuthenticationController extends Controller
 {
@@ -15,25 +16,21 @@ class AuthenticationController extends Controller
      * Maneja la peticion de creacion de nuevo usuario
      * 
      */
-    public function register (Request $request)
+    public function register (StoreRedactaUserRequest $request)
     {
-        $this->authorize('create', RedactaUser::class);
+        $this->authorize('create', User::class);
         $validatedData = $this->validateRequest($request);
-        try {
-            $user = User::create([
-                'name' => $request->name,
-                'last_name' => $request->last_name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)       
-            ]);
-            return response()->json([
-                'status' => 201,
-                'message' => 'OK',
-                'data' => $user
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()], 500);
-        }
+        $user = User::create([
+            'name' => $request->name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)       
+        ]);
+        return response()->json([
+            'status' => 201,
+            'message' => 'OK',
+            'data' => $user
+        ], 200);
     }
 
     /**
@@ -42,30 +39,26 @@ class AuthenticationController extends Controller
      */
     public function login (Request $request)
     {
-        try {
-            if (!Auth::attempt($request->only('email', 'password'))){
-                return response()->json([
-                    'status' => 401,
-                    'message' => 'Email y/o contraseña inválidos'
-                ], 401);
-            }
-            $user = User::where('email', $request['email'])->firstOrFail();
-            $user->last_access = now();
-            $user->save();
-            $token = $user->createToken('auth_token')->plainTextToken;
+        if (!Auth::attempt($request->only('email', 'password'))){
             return response()->json([
-                'status' => 200,
-                'message' => 'OK',
-                'data' => [
-                    'access_token' => $token,
-                    'token_type' => 'Bearer',
-                    'username' => $user->name.' '.$user->last_name,
-                    'redacta_user_id' => $user->id,
-                ]
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()], 500);
+                'status' => 401,
+                'message' => 'Email y/o contraseña inválidos'
+            ], 401);
         }
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $user->last_access = now();
+        $user->save();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'status' => 200,
+            'message' => 'OK',
+            'data' => [
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'username' => $user->name.' '.$user->last_name,
+                'redacta_user_id' => $user->id,
+            ]
+        ], 200);
     }
     
     /**
@@ -74,37 +67,7 @@ class AuthenticationController extends Controller
      */
     public function logout (Request $request)
     {
-        try {
-            $request->user()->currentAccessToken()->delete();
-        } catch (\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()], 500);
-        }
-    }
-
-
-    private function validateRequest($request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',        
-            'last_name' => 'required|string|max:255',        
-            'email' => 'required|email|unique:redacta_users',              
-            'password' => 'required|confirmed|min:8',  
-            //'roleId' => 'sometimes|numeric'
-        ], [
-            'required' => 'El campo :attribute es requerido',
-            'numeric' => 'El campo :attribute debe ser un número',
-            'string' => 'El campo :attribute debe ser un string',
-            'password.confirmed' => 'La contraseña ingresada y su confirmación no coinciden',
-            'password.min' => 'La contraseña ingresada debe tener una longitud no menor a 8 caracteres',
-            'email.unique' => 'El email ingresado ya existe. Intente con otro distinto',
-            'max'=> 'El valor del campo :attribute debe tener una longitud inferior a 255 caracteres'
-        ], [
-            'email' => '"Email"',
-            'name' => '"Nombre"',
-            'last_name' => '"Apellido"',
-            'password' => '"Contraseña"',
-            //'roleId' => '"Rol"',
-        ])->stopOnFirstFailure(true);
-        return $validator->validate();
+        $request->user()->currentAccessToken()->delete();
     }
 }
 
