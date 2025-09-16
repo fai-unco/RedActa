b5a4d52360fd920dc347b726defc40ee499ae5d8
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-09-16 10:46:51 -0300

    (api) fix roles validations in user form requests

--- api/app/Http/Requests/UpdateRedactaUserRequest.php
+++ api/app/Http/Requests/UpdateRedactaUserRequest.php
@@ -28,7 +28,7 @@ class UpdateRedactaUserRequest extends FormRequest
             'last_name' => 'sometimes|string|max:255',
             'email' => 'sometimes|string|email|max:255|unique:redacta_users,email,'.$redactaUser->id,
             'password' => 'sometimes|string|min:8|confirmed',
-            'role' => 'sometimes|string|in:super_admin,local_admin,regular_user'
+            'role' => 'sometimes|string|in:super_admin,local_admin,editor'
         ];
     }
 }
