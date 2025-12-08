ae8ccfa2aa96766131b815eed7f3163e462fcc5c
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-12-08 17:13:37 -0300

    (api) add 'Document' model import in DocumentSharedAccessPolicy

--- api/app/Policies/DocumentSharedAccessPolicy.php
+++ api/app/Policies/DocumentSharedAccessPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\DocumentSharedAccess;
 use App\Models\RedactaUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Document;
 
 class DocumentSharedAccessPolicy
 {
