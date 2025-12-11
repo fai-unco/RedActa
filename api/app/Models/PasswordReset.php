23d4cbb42006a6dbe88b00326820a125c567bdf4
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-12-11 11:24:22 -0300

    (api) add 'PasswordReset' model

---
+++ api/app/Models/PasswordReset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    protected $table = 'password_resets';
+    public $timestamps = false;
+    protected $fillable = ['email', 'token', 'used', 'created_at'];
+}
