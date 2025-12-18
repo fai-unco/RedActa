2a37627390ce57b6648b2b409ba50b045f56abd7
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2025-12-18 16:11:45 -0300

    (api) update PasswordReset model to use 'token' field as primary key

--- api/app/Models/PasswordReset.php
+++ api/app/Models/PasswordReset.php
@@ -9,4 +9,5 @@ class PasswordReset extends Model
     protected $table = 'password_resets';
     public $timestamps = false;
     protected $fillable = ['email', 'token', 'used', 'created_at'];
+    protected $primaryKey = 'token';
 }
