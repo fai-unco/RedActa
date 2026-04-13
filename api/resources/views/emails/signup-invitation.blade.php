1a056b5928978f5c2024715571c9749b8dd8a653
nahuel.mamani <nahuel.mamani@debian-8-amd64.fi.uncoma.edu.ar>
2026-04-13 10:44:57 -0300

    (api) fix signup invitation email template

--- api/resources/views/emails/signup-invitation.blade.php
+++ api/resources/views/emails/signup-invitation.blade.php
@@ -1,5 +1,5 @@
-<strong>Hola {{$invitation->redactaUser->name}}</strong><br><br>
-Te hemos enviado una invitación para unirte a nuestra plataforma. Haz clic en el siguiente enlace para confirmar tu registro:<br><br>
+<strong>Hola!</strong><br><br>
+Te hemos enviado una invitación para unirte RedActa. Haz clic en el siguiente enlace para confirmar tu registro:<br><br>
 <a href="{{$url}}">{{$url}}</a>
 <br><br>
 Si no solicitaste esta invitación, puedes ignorar este correo electrónico.<br><br>
